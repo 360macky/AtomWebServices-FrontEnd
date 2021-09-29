11ce30277461b3d759546cbe7c179cb14c2c3470
vlady gutierrez <vlady4780@outlook.com>
2021-09-29 15:39:45 -0500

    v2-LoginRegister

--- src/Login/auth.js
+++ src/Login/auth.js
@@ -1,10 +1,34 @@
 import './style/auth.css'
+
 import backgroundImage from './style/preview.jpg'
 import {Link} from "react-router-dom"
 import Register from "./register"
+import * as React from 'react'
+
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import ButtonGroup  from '@mui/material/ButtonGroup'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+import { makeStyles } from '@mui/styles'
+
 const public_data = process.env.PUBLIC_URL;
 
+const useStyle = makeStyles({
+    link:{
+        color: "white",
+        textDecoration: 'none',
+        '&:hover':{ 
+            color: "white"
+        },
+    },
+    textField:{
+        color: "#ffffff !important"
+    }
+})
+
 export const Login = () =>{
+    const classes = useStyle()
     return (
         <div className="container_box">
             <div className="login_container">
@@ -13,17 +37,36 @@ export const Login = () =>{
                     <img className="img-fluid" src={public_data+"/img/book.png"}/>
                 </div>
                 <div className="login_Right">
-                    <h1>Login</h1>
-                    <form>
-                        <input type="text" placeholder="Username"/>
-                        <input type="password" placeholder="Password"/>
+                    <h1 >Login</h1>
+                    <Box component="form"
+                        sx={{
+                            '& .MuiTextField-root': { m: 1, width: '25ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                        >
+                        <TextField InputProps={{
+                            classes: {
+                            input: classes.textField
+                        }
+                        }} placeholder="Please enter your username" id="input_user" label="Username" variant="standard" color="success"  />
+                        <TextField InputProps={{
+                            classes: {
+                            input: classes.textField
+                        }}} placeholder="Please enter your password" id="input_pass" type="password" autoComplete="current-password" label="Password" variant="standard" color="success"/>
                         <div className="btn_options">
                             <div className="container_btn_options">
-                                <button className="btn-submit" type="submit">Login</button>
-                                <Link to="/register" className="btn-register">Registro</Link>
+                                <Stack spacing={2} direction="row">
+                                    <Button variant="contained" color="success" fullWidth>Login</Button>
+                                    <Button variant="contained" color="info" fullWidth>
+                                        <Link className={classes.link} to="/register">
+                                            Register
+                                        </Link>
+                                    </Button>
+                                </Stack>
                             </div>
                         </div>
-                    </form>
+                    </Box>
                 </div>
             </div>
         </div>
--- src/Login/register.js
+++ src/Login/register.js
@@ -1,34 +1,83 @@
-import './style/auth.css';
+import './style/auth.css'
+
 import backgroundImage from './style/preview.jpg'
 import {Link} from "react-router-dom"
+import Login from "./auth"
+import * as React from 'react'
+
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import ButtonGroup  from '@mui/material/ButtonGroup'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+import { makeStyles } from '@mui/styles'
 
 const public_data = process.env.PUBLIC_URL;
 
+const useStyle = makeStyles({
+    link:{
+        color: "white",
+        textDecoration: 'none',
+        '&:hover':{ 
+            color: "white"
+        },
+    },
+    textField:{
+        color: "#ffffff !important"
+    }
+})
+
 export const Register = () =>{
-    return (
-        <div className="container_box">
-            <div className="login_container">
-                <div className="login_Left">
-                    <h1>Ingreso a almacén</h1>
-                    <img className="img-fluid" src={public_data+"/img/book.png"}/>
-                </div>
-                <div className="login_Right">
-                    <h1>Registro de datos</h1>
-                    <form>
-                        <input type="text" placeholder="Name"/>
-                        <input type="text" placeholder="Age"/>
-                        <input type="text" placeholder="Username"/>
-                        <input type="password" placeholder="Password"/>
-                        <div className="btn_options">
-                            <div className="container_btn_options">
-                                <button className="btn-register" type="button">Register</button>
-                                <Link to="/" className="btn-submit">Volver</Link>
+    const classes = useStyle()
+        return (
+            <div className="container_box">
+                <div className="login_container">
+                    <div className="login_Left">
+                        <h1>Ingreso a almacén</h1>
+                        <img className="img-fluid" src={public_data+"/img/book.png"}/>
+                    </div>
+                    <div className="login_Right">
+                        <h1 >Registro</h1>
+                        <Box component="form"
+                            sx={{
+                                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                            >
+                            <TextField InputProps={{
+                                classes: {
+                                input: classes.textField
+                            }
+                            }} id="input_user" label="Username" variant="standard" color="success"  />
+                            <TextField InputProps={{
+                                classes: {
+                                input: classes.textField
+                            }}} id="input_name" autoComplete="current-password" label="Name" variant="standard" color="success"/>
+                            <TextField InputProps={{
+                                classes: {
+                                input: classes.textField
+                            }}} id="input_lastname" autoComplete="current-password" label="Lastname" variant="standard" color="success"/>
+                            <TextField InputProps={{
+                                classes: {
+                                input: classes.textField
+                            }}} id="input_pass" type="password" autoComplete="current-password" label="Password" variant="standard" color="success"/>
+                            <div className="btn_options">
+                                <div className="container_btn_options">
+                                    <Stack spacing={2} direction="row">
+                                        <Button variant="contained" color="success" fullWidth>Register</Button>
+                                        <Button variant="contained" color="error" fullWidth>
+                                            <Link className={classes.link} to="/">
+                                                Back
+                                            </Link>
+                                        </Button>
+                                    </Stack>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </Box>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
 }
 export default Register
