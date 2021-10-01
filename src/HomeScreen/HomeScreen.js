d49c650ce1aba71bee4f5c1280c76a26ece8775a
Marcelo Arias <marias@bitorical.com>
2021-09-30 19:17:12 -0500

    Agregar estilos a HomeScreen.js

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
+import './style/HomeScreen.css'
+import ProductScreen from '../ProductScreen/ProductScreen'
 
 export default class HomeScreen extends Component {
     render() {
         return (
             <section className="HomeScreen">
-                <h1>Dashboard</h1>
-                <div>
-                    
+              <h1 className="HomeScreen__title">Dashboard</h1>
+                <div className="HomeScreen__buttons">
+                    <button className="HomeScreen__button">Inicio</button>
+                    <button className="HomeScreen__button">Refrescar los datos</button>
+                    <button className="HomeScreen__button">Cerrar sesión</button>
+                </div>
+                <div className="HomeScreen__products">
+                    Cargando productos...
                 </div>
             </section>
         )
